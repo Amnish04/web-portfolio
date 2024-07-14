2f9d83f4a9ae80ad6603291231ccc5756171a771
Amnish <amnishsingh04@gmail.com>
2024-07-14 12:31:10 -0400

    Create Experience model

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,10 +1,10 @@
 export class Project {
-    public name: string;
-    public description: string;
-    public technologiesUsed: string[];
-    public imageUrl: string;
-    public githubLink: string;
-    public deployedLink: string;
+    name: string;
+    description: string;
+    technologiesUsed: string[];
+    imageUrl: string;
+    githubLink: string;
+    deployedLink: string;
 
     constructor(
         name: string,
